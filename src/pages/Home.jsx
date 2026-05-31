ec23ec4d2f94eda36db5f78925cfb4951e5010e3
Narendra-Git1 <narendray353@gmail.com>
2026-05-31 12:37:23 +0530

    Initial React frontend setup with Vite, Tailwind CSS, and React Router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+
+      <h1 className="text-5xl font-bold text-blue-600">
+        TechShopPro
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+        E-Commerce Full Stack Application
+      </p>
+
+    </div>
+  );
+}
+
+export default Home;
